6ed2e2cb246004217d743423c9de7fa0e81eaf18
Kady <74194999+Kady2020@users.noreply.github.com>
2022-02-17 11:54:13 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Описание проекта
+[![Header](https://github.com/Kady2020/relvise/blob/main/img/github-logo.png)](https://kady2020.github.io/relvise/)
---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+import * as flsFunctions from "./modules/webp.js";
+
+flsFunctions.isWebp();
+
+import "./libraries/jquery-3.6.0.min.js";
+import "./libraries/slick.min.js";
+import "./modules/slick-slider.js";
+import "./modules/burger-menu.js";
+import "./modules/header-scroll.js";
+import "./modules/spoiler-accordion.js";
+
