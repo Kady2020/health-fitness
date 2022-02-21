c5d38dca2b60766388f3d8a8320d4e1deadb5877
Kady <74194999+Kady2020@users.noreply.github.com>
2022-02-22 00:19:45 +0300

    fixed menu-link on header block and input on intro block

--- src/js/modules/burger-menu.js
+++ src/js/modules/burger-menu.js
@@ -3,10 +3,8 @@ $(document).ready(function () {
     $(".header,.header__menu,.header__menu-link,.header__authorization,.header__burger").toggleClass("active");
     $("body").toggleClass("lock");
   });
-});
 
-$(document).ready(function () {
-  $('.header__menu-link').click(function (event) {
+  $('.header__menu-link,.header__sign-in,.header__sign-up').click(function (event) {
     $(".header,.header__menu,.header__menu-link,.header__authorization,.header__burger").removeClass('active');
     $('body').removeClass('lock')
   });
