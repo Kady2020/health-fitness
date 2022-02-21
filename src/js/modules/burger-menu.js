c1fe162a736e7a291462c266503093d97296873d
Kady <74194999+Kady2020@users.noreply.github.com>
2022-02-21 09:48:23 +0300

    added remove class to header link

--- src/js/modules/burger-menu.js
+++ src/js/modules/burger-menu.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
   $(".header__burger").click(function (event) {
-    $(".header,.header__menu,.header__authorization,.header__burger").toggleClass("active");
+    $(".header,.header__menu,.header__menu-link,.header__authorization,.header__burger").toggleClass("active");
     $("body").toggleClass("lock");
+  });
+});
+
+$(document).ready(function () {
+  $('.header__menu-link').click(function (event) {
+    $(".header,.header__menu,.header__menu-link,.header__authorization,.header__burger").removeClass('active');
+    $('body').removeClass('lock')
   });
 });
