74c38d747ce89f914e06bfdcd5612ca0f21fa6e5
Kady <74194999+Kady2020@users.noreply.github.com>
2022-02-22 15:21:25 +0300

    completed block how it works

--- src/js/modules/spoiler-accordion.js
+++ src/js/modules/spoiler-accordion.js
@@ -1,9 +1,9 @@
 /*====================SPOILER & ACCORDION====================*/
 $(document).ready(function () {
-  $('.block__title').click(function (event) {
-    if ($('.block').hasClass('one')) {
-      $('.block__title').not($(this)).removeClass('active');
-      $('.block__text').not($(this).next()).slideUp(300);
+  $('.spoiler__title').click(function (event) {
+    if ($('.spoiler').hasClass('one')) {
+      $('.spoiler__title').not($(this)).removeClass('active');
+      $('.spoiler__text').not($(this).next()).slideUp(300);
     }
     $(this).toggleClass('active').next().slideToggle(300);
   });
